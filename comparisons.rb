c4eaf48ecae68f13b24aef0690b088c96926a4ce
Billy Burch <billyburch40@gmail.com>
2023-08-01 12:19:29 -0600

    Add remaining comparisons

--- comparisons.rb
+++ comparisons.rb
@@ -34,15 +34,19 @@ puts "is number_teachers not equal to number_students?", number_teachers != numb
 # this should print: true
 
 # YOU DO: print the result of the comparison: is number_students greater than or equal to 20?
+puts "is number_students greater than or equal to 20?", number_students >= 20
 # this should print: true
 
 # YOU DO: print the result of the comparison: is number_students greater than or equal to 21?
+puts "is number_students greater than or equal to 21?", number_students >= 21
 # this should print: false
 
 # YOU DO: print the result of the comparison: is number_students less than or equal to 20?
+puts "is number_students less than or equal to 20?", number_students <= 20
 # this should print: true
 
 # YOU DO: print the result of the comparison: is number_students less than or equal to 21?
+puts "is number_students less than or equal to 21?", number_students <= 21
 # this should print: true
 
 
